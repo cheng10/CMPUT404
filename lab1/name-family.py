a5be6e6ee27b39c7a53c984f9ab7af4c0daa2e4e
cheng10 <cheng10@ualberta.ca>
2016-01-12 20:19:38 -0700

    finish the python class example

--- lab1/name-family.py
+++ lab1/name-family.py
@@ -1,15 +1,19 @@
 class Student:
 	courseMarks={}
 	name=""
-	def _int_(self, name, family):
+	family=""
+	def __init__(self, name, family):
 		self.name=name
 		self.family=family
 	def addCourseMark(self, course, mark):
-		self.courseMark[course]=mark
+		self.courseMarks[course]=mark
 	def average(self):
-		return sum(self.courseMark.values())
-
-jack=Student("Jack", "Chen")
-jack.addCourseMark(Math101,100)
-jack.addCourseMark(Math102,80)
-print jack.average(self)
+		return sum(self.courseMarks.values())/len(self.courseMarks)
+	
+if __name__ == "__main__": 
+	jack=Student("Jack", "Chen")
+	jack.addCourseMark("Math101",100)
+	jack.addCourseMark("Math102",80)
+	print "Name:" + jack.name
+	print "Family:" + jack.family
+	print "Average:" +str(jack.average())
